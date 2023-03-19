e86d2c139cb05e6eb8c1668d4a6bf3e6232eb967
andika <yuda.yokesen@gmail.com>
2023-03-19 15:11:24 +0700

    users invoice

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -15,17 +16,29 @@ class AdminController extends Controller
     public function login(Request $request)
     {
         // dd($request);
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            if (Auth::user()->role_id == 1) {
-                return redirect()->route('dashboard');
-            } elseif (Auth::user()->role_id == 2) {
-                return redirect()->route('getOrders');
-            } elseif (Auth::user()->role_id == 3) {
-                return redirect()->route('liveOrder');
+
+        // Check if user aktif
+        $user = DB::table('users')->where('email', $request->email)->first();
+        if ($user) {
+            if ($user->is_active == 0) {
+                alert('Gagal', 'Email Tidak Ditemukan', 'error');
+                return redirect()->back();
+            }
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
+                if (Auth::user()->role_id == 1) {
+                    return redirect()->route('dashboard');
+                } elseif (Auth::user()->role_id == 2) {
+                    return redirect()->route('getOrders');
+                } elseif (Auth::user()->role_id == 3) {
+                    return redirect()->route('liveOrder');
+                }
+            } else {
+                alert('Gagal', 'Kombinasi Email Dan Password Salah', 'error');
+                return redirect()->back();
             }
         } else {
-            alert('Gagal', 'Kombinasi Email Dan Password Salah', 'error');
+            alert('Gagal', 'Email Tidak Ditemukan', 'error');
             return redirect()->back();
         }
     }
